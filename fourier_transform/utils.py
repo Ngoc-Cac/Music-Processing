b5dd0efcb0179e317cd9ddfb9f4c65eedd27f279
Trịnh Ngọc Các <cactrinh5@gmail.com>
2025-03-08 16:21:40 +0700

    Update utils.py
    added function to compute frequencies of midi notes

--- fourier_transform/utils.py
+++ fourier_transform/utils.py
@@ -1,5 +1,15 @@
 import numpy as np
 
+from numbers import Number
+
+def midi_pitch(
+    note_number: Number,
+    *,
+    reference_pitch: Number = 440,
+    reference_note: int = 69
+):
+    return 2 ** ((note_number - reference_note)/ 12) * reference_pitch
+
 def init_dft_mat(dimension: int):
     if not isinstance(dimension, int):
         raise TypeError("'dimension' must be a positive integer")
@@ -14,4 +24,3 @@ def init_dft_mat(dimension: int):
 
 def init_twiddle_factors(dimension: int):
     return np.fromiter((np.exp(-2j * (np.pi / dimension) * i) for i in range(dimension // 2)), dtype=np.complex128)
-
