4737c1bbccebd9fb4ce2c5dbedec990e00f5fcb8
Trịnh Ngọc Các <cactrinh5@gmail.com>
2025-03-08 21:06:10 +0700

    minor refactor
    refactored code for computing the log-frequency scale as well as define some constant

--- fourier_transform/spectrogram.py
+++ fourier_transform/spectrogram.py
@@ -1,13 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-from fourier_transform.utils import midi_pitch
+from fourier_transform.utils import midi_pitch, convert_to_decibels
 
 from numpy.typing import NDArray
 from numbers import Number
 from matplotlib.axes import Axes
 from typing import Optional
 
+
+_MIDI_NOTE_RANGE = np.arange(128)
 
 def _compute_LF(
     stft_coefs: NDArray[np.complex128],
@@ -20,10 +22,10 @@ def _compute_LF(
     lf_coefs = np.zeros((128, stft_coefs.shape[1]), dtype=np.float64)
     all_frequencies = np.arange(stft_coefs.shape[0]) * sampling_rate / window_length
 
-    for i in range(128):
-        lower = midi_pitch(i - 0.5, reference_pitch=reference_pitch)
-        upper = midi_pitch(i + 0.5, reference_pitch=reference_pitch)
-        pitch_i = stft_coefs[(lower <= all_frequencies) & (all_frequencies <= upper)]
+    lower = midi_pitch(_MIDI_NOTE_RANGE - 0.5, reference_pitch=reference_pitch)
+    upper = midi_pitch(_MIDI_NOTE_RANGE + 0.5, reference_pitch=reference_pitch)
+    for i in _MIDI_NOTE_RANGE:
+        pitch_i = stft_coefs[(lower[i] <= all_frequencies) & (all_frequencies <= upper[i])]
         lf_coefs[i] = pitch_i.sum(axis=0)
     return lf_coefs
 
@@ -41,17 +43,20 @@ def spectrogram(
     vmax: Optional[float] = None,
     ax: Optional[Axes] = None
 ) -> Axes:
-    if ax is None:
-        ax = plt.subplot(111)
     intensities = np.abs(stft_coefs) ** 2
     if show_decibels:
-        intensities = 10 * (np.log10(intensities + np.finfo(float).eps))
+        intensities = convert_to_decibels(intensities)
+
+    
+    if ax is None:
+        ax = plt.subplot(111)
 
     right = (intensities.shape[1] * hop_size + window_length) / sampling_rate\
             if show_realtime else intensities.shape[1]
     upper = intensities.shape[0] * sampling_rate / window_length\
             if show_frequency else intensities.shape[0]
-    img = ax.imshow(intensities, origin='lower', cmap=cmap, aspect='auto',
+    img = ax.imshow(intensities, origin='lower', aspect='auto',
+                    cmap=cmap,
                     vmin=vmin, vmax=vmax,
                     extent=[0, right, 0, upper])
     
@@ -59,7 +64,7 @@ def spectrogram(
     ax.set_xlabel(f'Time ({'seconds' if show_realtime else 'frames'})')
     ax.get_figure().colorbar(img, orientation='vertical')\
                    .set_label(f"Magnitude {'(dB)' if show_decibels else ''}")
-    return ax, img
+    return ax
 
 def LF_spectrogram(
     stft_coefs: NDArray[np.complex128],
@@ -75,18 +80,19 @@ def LF_spectrogram(
     reference_pitch: Number = 440,
     ax: Optional[Axes] = None
 ) -> Axes:
-    if ax is None:
-        ax = plt.gca()
-
     lf_coefs = _compute_LF(stft_coefs, sampling_rate, window_length,
                            reference_pitch=reference_pitch)
     if show_decibels:
-        # + eps to avoid division by zero
-        lf_coefs = 10 * (np.log10(lf_coefs + np.finfo(float).eps))
+        intensities = convert_to_decibels(intensities)
+
+
+    if ax is None:
+        ax = plt.subplot(111)
 
     right = (lf_coefs.shape[1] * hop_size + window_length) / sampling_rate\
             if show_realtime else lf_coefs.shape[1]
-    img = ax.imshow(lf_coefs, origin='lower', cmap=cmap, aspect='auto',
+    img = ax.imshow(lf_coefs, origin='lower', aspect='auto',
+                    cmap=cmap,
                     vmin=vmin, vmax=vmax,
                     extent=[0, right, 0, 128])
     
@@ -94,7 +100,7 @@ def LF_spectrogram(
     ax.set_xlabel(f'Time ({'seconds' if show_realtime else 'frames'})')
     cbar = ax.get_figure().colorbar(img, orientation='vertical')
     cbar.set_label(f'Magnitude {'(dB)' if show_decibels else ''}')
-    return ax, img
+    return ax
 
 def chromagram(
     stft_coefs: NDArray[np.complex128],
@@ -110,24 +116,23 @@ def chromagram(
     reference_pitch: Number = 440,
     ax: Optional[Axes] = None
 ) -> Axes:
-    if ax is None:
-        ax = plt.gca()
-
     lf_coefs = _compute_LF(stft_coefs, sampling_rate, window_length,
                            reference_pitch=reference_pitch)
-    
-    midi_range = np.arange(128)
     chroma_coefs = np.zeros((12, lf_coefs.shape[1]))
     for chroma in range(12):
-        chroma_coefs[chroma] = lf_coefs[(midi_range % 12) == chroma].sum(axis=0)
+        chroma_coefs[chroma] = lf_coefs[(_MIDI_NOTE_RANGE % 12) == chroma].sum(axis=0)
 
     if show_decibels:
-        # + eps to avoid division by zero
-        chroma_coefs = 10 * (np.log10(chroma_coefs + np.finfo(float).eps))
+        intensities = convert_to_decibels(intensities)
 
+
+    if ax is None:
+        ax = plt.subplot(111)
+        
     right = (chroma_coefs.shape[1] * hop_size + window_length) / sampling_rate\
             if show_realtime else chroma_coefs.shape[1]
-    img = ax.imshow(chroma_coefs, origin='lower', cmap=cmap, aspect='auto',
+    img = ax.imshow(chroma_coefs, origin='lower', aspect='auto',
+                    cmap=cmap,
                     vmin=vmin, vmax=vmax,
                     extent=[0, right, 0, 12])
     
@@ -135,4 +140,4 @@ def chromagram(
     ax.set_xlabel(f'Time ({'seconds' if show_realtime else 'frames'})')
     cbar = ax.get_figure().colorbar(img, orientation='vertical')
     cbar.set_label(f'Magnitude {'(dB)' if show_decibels else ''}')
-    return ax, img
+    return ax
