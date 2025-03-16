89665b1407f6d8609734bcd04d45d6e47c48290d
Trịnh Ngọc Các <cactrinh5@gmail.com>
2025-03-16 10:14:29 +0700

    added smoothing and downsampling
    added smoothing in time domain by convoluting the matrix wrt a window and taking average
    downsampling is down by taking every nth sample

--- MuPro/utils/mat_op.py
+++ MuPro/utils/mat_op.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+from scipy import signal
 
 from numpy.typing import NDArray
 
@@ -19,3 +19,23 @@ def normalize_column(
     normalized_mat = matrix / mags
     normalized_mat[:, mags < threshold] = unit_vec
     return normalized_mat
+
+def smooth_mat(
+    matrix: NDArray,
+    filter_len: int,
+    window: str = 'boxcar'
+):
+    kernel_func = signal.windows.get_window(window, filter_len)[None, :]
+    return signal.convolve(matrix, kernel_func, 'same') / np.sum(kernel_func)
+
+def downsample(
+    matrix: NDArray,
+    hop_size: int,
+    axis: int = 1
+):
+    if axis == 0:
+        return matrix[::hop_size]
+    elif axis == 1:
+        return matrix[:, ::hop_size]
+    else:
+        raise ValueError('Weird value for `axis` argument.')
